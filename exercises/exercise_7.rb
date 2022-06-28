fa8d7b571ac503f71034fce7a6c9eb7fb2bbebb6
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-28 10:23:40 -0700

    complete final exervice

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,16 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+Employee.create(hourly_rate: 850).errors.each do |e|
+  puts e.full_message
+end
+
+input = $stdin.gets.chomp
+
+Store.create(name: "Vegas").errors.each do |e|
+  puts e.full_message
+end 
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_numericality_of :hourly_rate 
+  validates_numericality_of :hourly_rate, {minimum: 40}
+  validates_numericality_of :hourly_rate, {maximum: 200}
 end
+
