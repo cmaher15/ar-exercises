84255a906bf171cccbd3354f2fc7edb2b751298b
cmaher15 <ceilidhemaher@hotmail.com>
2022-06-27 20:02:25 -0700

    completed exercise 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -12,6 +12,3 @@ puts "----------"
 
 puts Store.new
 
-# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
-# 2. Load the second store from the database and assign it to `@store2`.
-# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
